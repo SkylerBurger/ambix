e913db709795308cd4b37d8a62c4b14065e666eb
Skyler Burger <skylerburger@gmail.com>
2024-04-09 16:26:56 -0700

    Adds deleting tracks

--- src/Youtube/Youtube.js
+++ src/Youtube/Youtube.js
@@ -2,52 +2,110 @@ import useYouTube from './useYouTube.js';
 import Volume from '../Controls/Volume/Volume.js';
 import "./Youtube.css";
 import PlayButton from '../Controls/PlayButton/PlayButton.js';
+import { useState } from 'react';
 
-const AmbientTrack = ({ name, trackVideoId, changeTrackFunc, selected }) => {
+const AmbientTrack = ({ buttonKey, name, trackVideoId, changeTrackFunc, selected }) => {
+  const selectedClassName = 'track' + (selected ? ' selected': '');
   return (
     <button 
-      onClick={() => changeTrackFunc(trackVideoId)}
-      className={ selected ? 'selected' : ''}>
+      onClick={() => changeTrackFunc(buttonKey, trackVideoId)}
+      className={ selectedClassName }>
         { name }
     </button>
   )
 }
 
+const AddTrackModal = ({ isVisible, setIsVisible, tracklist, setTracklist }) => {
+  const [formValues, setFormValues] = useState({});
+  const modalClass = 'add-track' + (isVisible ? '' : ' hidden');
+
+  const changeHandler = (event) => {
+    const newFormValues = {
+      ...formValues,
+      [event.target.name]: event.target.value,
+    };
+    setFormValues(newFormValues);
+  };
+  
+  const handleAddTrack = () => {
+    const extractIdExp = /[a-zA-Z0-9-_]{11}/;
+    const newVideoId = formValues.newTrackLink.match(extractIdExp)[0];
+
+    const newTracklist = [
+      ...tracklist, 
+      { trackName: formValues.newTrackName, trackVideoId: newVideoId }
+    ];
+
+    setTracklist(newTracklist);
+    setIsVisible(false);
+    setFormValues({ newTrackName: '', newTrackLink: '' });
+    localStorage.setItem('ambient-tracklist', JSON.stringify(newTracklist))
+  }
+
+  return (
+    <div className={ modalClass }>
+      <div className="new-track-info">
+        <input
+          type="text"
+          name="newTrackName"
+          placeholder="Track Name"
+          value={ formValues.newTrackName }
+          onChange={ changeHandler }
+        />
+        <input
+          type="text"
+          name="newTrackLink"
+          placeholder="YouTube Link"
+          value={ formValues.newTrackLink }
+          onChange={ changeHandler }
+        />
+      </div>
+      <button onClick={ handleAddTrack }>Add Track</button>
+      <button onClick={ () => setIsVisible(false) }>Cancel</button>
+    </div>
+  );
+};
+
 const YouTube = () => {
   const {
     changePlayerVolume,
     changeTrack,
+    currentButtonKey,
+    deleteTrack,
+    isAddModalVisible,
+    setIsAddModalVisible,
     isPaused,
-    isTrackLoaded,
-    playerVideoId, 
+    isTrackLoaded, 
     rangeValue,
     togglePlayback,
-    volumeLevel, 
+    volumeLevel,
+    tracklist,
+    setTracklist, 
   } = useYouTube();
-
-  const defaultTracks = [
-    {trackName: "cafe", trackVideoId: "gaGrHUekGrc"},
-    {trackName: "campfire", trackVideoId: "QMJYlmX1sNU"},
-    {trackName: "fireplace", trackVideoId: "K0pJRo0XU8s"},
-    {trackName: "lab", trackVideoId: "eGeJF85SOdQ"},
-    {trackName: "rain", trackVideoId: "LlKyGAGHc4c"},
-    {trackName: "storm", trackVideoId: "EbMZh-nQFsU"},
-    {trackName: "waves", trackVideoId: "ibZUd-6pDeY"},
-  ]
 
   return (
     <section className='youtube-player media-module'>
       <h2>youtube</h2>
       <div id='player'></div>
       <div className='ambience-tracks'>
-        { defaultTracks.map(({trackName, trackVideoId}) => {
-            return <AmbientTrack 
-              name={ trackName } 
-              trackVideoId={ trackVideoId} 
-              changeTrackFunc={ changeTrack }
-              selected={playerVideoId === trackVideoId}/>
+        { tracklist.map(({trackName, trackVideoId}, i) => {
+            return <AmbientTrack
+              buttonKey={i} 
+              name={trackName} 
+              trackVideoId={trackVideoId} 
+              changeTrackFunc={changeTrack}
+              selected={i === currentButtonKey}/>
           })
         }
+        <i className="fas fa-plus track-buttons"
+          onClick={ ()=> setIsAddModalVisible(!isAddModalVisible) }></i>
+        <i className={`fa fa-trash-o track-buttons ${currentButtonKey ? '' : 'no-display'}`} 
+          onClick={deleteTrack}></i>
+        <AddTrackModal 
+          isVisible={isAddModalVisible}
+          setIsVisible={setIsAddModalVisible}
+          tracklist={tracklist} 
+          setTracklist={setTracklist} />
       </div>
       <div className='player-controls'>
         <PlayButton
@@ -62,7 +120,7 @@ const YouTube = () => {
           volumeLevel={volumeLevel} />
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default YouTube;
--- src/Youtube/useYouTube.js
+++ src/Youtube/useYouTube.js
@@ -1,14 +1,30 @@
 import { useEffect, useRef, useState } from "react";
 
 
+const defaultTracklist = [
+  {trackName: "cafe", trackVideoId: "gaGrHUekGrc"},
+  {trackName: "campfire", trackVideoId: "QMJYlmX1sNU"},
+  {trackName: "fireplace", trackVideoId: "K0pJRo0XU8s"},
+  {trackName: "lab", trackVideoId: "eGeJF85SOdQ"},
+  {trackName: "rain", trackVideoId: "LlKyGAGHc4c"},
+  {trackName: "storm", trackVideoId: "EbMZh-nQFsU"},
+  {trackName: "waves", trackVideoId: "ibZUd-6pDeY"},
+]
+
+const saveTracklist =(tracklist) => {
+  localStorage.setItem('ambient-tracklist', JSON.stringify(tracklist));
+};
+
 function useYouTube() {
   const playerRef = useRef(null);
   const intervalRef = useRef(0);
   const [isPaused, setIsPaused] = useState(true);
   const [isTrackLoaded, setIsTrackLoaded] = useState(false);
-  const [playerVideoId, setPlayerVideoId] = useState('');
   const [rangeValue, setRangeValue] = useState(0.5);
   const [volumeLevel, setVolumeLevel] = useState(50);
+  const [isAddModalVisible, setIsAddModalVisible] = useState(false);
+  const [tracklist, setTracklist] = useState([]);
+  const [currentButtonKey, setCurrentButtonKey] = useState(null);
 
   const createPlayer = () => {
     playerRef.current = new window.YT.Player(
@@ -21,59 +37,83 @@ function useYouTube() {
   }
 
   useEffect(() => {
+    if (localStorage.getItem('ambient-tracklist') !== null) {
+      const tracklistString = localStorage.getItem('ambient-tracklist');
+      setTracklist(JSON.parse(tracklistString));
+    } else {
+      saveTracklist(defaultTracklist)
+      setTracklist(defaultTracklist);
+    }
+
     const checkForAPI = () => {
       if (window.YT !== null) {
         clearInterval(intervalRef.current);
         createPlayer();
       }
-    }
+    };
+
     intervalRef.current = setInterval(checkForAPI, 500);
   }, []);
 
   const pause = () => {
     playerRef.current.stopVideo();
     setIsPaused(true);
-  }
+  };
   
   const resume = () => {
     playerRef.current.playVideo();
     setIsPaused(false);
-  }
+  };
 
   const togglePlayback = () => {
     isPaused ? resume() : pause();
-  }
+  };
 
-  const changeTrack = (newVideoId) => {
-    if (playerVideoId === newVideoId) {
+  const changeTrack = (newButtonKey, newVideoId) => {
+    if (currentButtonKey === newButtonKey) {
       playerRef.current.stopVideo();
-      setPlayerVideoId('');
+      setCurrentButtonKey(null);
       setIsTrackLoaded(false);
       setIsPaused(true);
     } else {
       playerRef.current.loadVideoById(newVideoId);
       playerRef.current.playVideo();
-      setPlayerVideoId(newVideoId);
+      setCurrentButtonKey(newButtonKey);
       setIsTrackLoaded(true);
       setIsPaused(false);
     }
-  }
+  };
 
   const changePlayerVolume = (newVolumeLevel, newRangeValue) => {
     playerRef.current.setVolume(newVolumeLevel);
     setVolumeLevel(newVolumeLevel);
     setRangeValue(newRangeValue);
-  }
+  };
+  
+  const deleteTrack = () => {
+    const newTracklist = tracklist.filter((track, index) => currentButtonKey !== index);
+    playerRef.current.stopVideo();
+    saveTracklist(newTracklist)
+    setTracklist(newTracklist);
+    setCurrentButtonKey(null);
+    setIsPaused(true);
+    setIsTrackLoaded(false);
+  };
 
   return {
     changePlayerVolume,
     changeTrack,
+    currentButtonKey,
+    deleteTrack,
+    isAddModalVisible,
+    setIsAddModalVisible,
     isPaused,
-    isTrackLoaded, 
-    playerVideoId, 
+    isTrackLoaded,
     rangeValue,
     togglePlayback,
     volumeLevel,
+    tracklist,
+    setTracklist,
   };
 }
 
