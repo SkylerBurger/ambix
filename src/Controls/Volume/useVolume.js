41f969d28a62deac34464972fdb8710497aeb50d
Skyler Burger <skylerburger@gmail.com>
2024-10-08 14:26:49 -0700

    Removes comments and console logs

--- src/Controls/Volume/useVolume.js
+++ src/Controls/Volume/useVolume.js
@@ -5,31 +5,18 @@ const useVolume = (changePlayerVolume, isTrackLoaded, rangeValue) => {
 
   const rangeHandler = (event) => {
     if (!isTrackLoaded) return;
-
-    console.log(`*** current range value ${rangeValue}`);
-
     const newRangeValue = parseFloat(event.target.value);
-    console.log(`*** new range value ${newRangeValue}`);
-
     const newVolumeLevel = adjustForHumans(newRangeValue);
-    console.log(`*** new volume level ${newVolumeLevel}`);
-
     changePlayerVolume(newVolumeLevel, newRangeValue);
   };
 
   const volumeButtonHandler = (increment) => {
     if (!isTrackLoaded) return;
-
-    console.log(`*** current range value ${rangeValue}`);
-    console.log(`*** Increment ${increment} Type ${typeof increment}`);
     let newRangeValue = rangeValue + increment;
     if (newRangeValue < 0 || newRangeValue > 100) {
       newRangeValue = newRangeValue > 100 ? 100 : 0;
     }
-    console.log(`*** new range value ${newRangeValue}`);
-
     const newVolumeLevel = adjustForHumans(newRangeValue);
-    console.log(`*** new volume level ${newVolumeLevel}`);
     changePlayerVolume(newVolumeLevel, newRangeValue);
   };
 
